0daf81691d39f54b960ff4216bb26fcf7329a395
Lenya <leonidivanov826@gmail.com>
2026-06-13 12:02:08 +0300

    Initial Commit

---
+++ Habit-Tracking-Test-App/Habit_Tracking_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_Tracking_Test_AppApp.swift
+//  Habit-Tracking-Test-App
+//
+//  Created by Леонід Іванов on 13.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Habit_Tracking_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
